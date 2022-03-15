9c57ba61b0af49d79bf5ac29715655124f910000
ksyushakk <101617997+ksyushakk@users.noreply.github.com>
2022-03-15 09:41:30 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const DB_HOSTNAME = '89.108.83.110';
+const DB_USERNAME = 'is342_12';
+const DB_PASSWORD = 'ZnAu5hbZH9';
+const DB_DATABASE = 'is342_12';
+const DB_PORT = '3306'; # Default 3306
